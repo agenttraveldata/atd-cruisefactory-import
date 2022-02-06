a2f4ebfad91dcb74792fda700bc906368b745e40
Christopher Merat <chris@merat.com.au>
2022-02-06 14:41:31 +1100

    Updated stimulus to v3.

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
-import "./styles/styles.scss"
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+import "./styles/styles.scss";
+import {Application} from "@hotwired/stimulus";
+import {definitionsFromContext} from "@hotwired/stimulus-webpack-helpers";
 
-const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
+window.Stimulus = Application.start();
+const context = require.context("./controllers", true, /\.js$/);
+Stimulus.load(definitionsFromContext(context));
