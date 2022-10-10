44a851b1259839d48460f6e72572929eb3b8025e
Christopher Merat <chris@agenttraveldata.com>
2022-10-10 11:16:34 +1100

    Changed search options find valid options excluding its own value.

--- src/Controller/Api/v1/SearchController.php
+++ src/Controller/Api/v1/SearchController.php
@@ -27,13 +27,141 @@ class SearchController extends AbstractController {
 	}
 
 	public function options( WP_REST_Request $request ): array {
-		$taxQuery = [];
-        $metaQuery[ Month::$name . '_baseline' ] = [
-            'key'     => 'atd_cfi_sailing_date',
-            'value'   => (new DateTime())->getTimestamp(),
-            'type'    => 'NUMERIC',
-            'compare' => '>='
-        ];
+		$terms = [];
+
+		foreach ( get_object_taxonomies( Departure::$postType ) as $taxonomy ) {
+			$atd = substr( $taxonomy, 0, 11 );
+
+			if ( $atd === 'atd_cf_tax_' ) {
+				$postQuery = null;
+				$taxQuery  = $metaQuery = [];
+
+				$metaQuery[ Month::$name . '_baseline' ] = [
+					'key'     => 'atd_cfi_sailing_date',
+					'value'   => ( new DateTime() )->getTimestamp(),
+					'type'    => 'NUMERIC',
+					'compare' => '>='
+				];
+
+				foreach ( $request->get_query_params() as $param => $value ) {
+					if ( empty( $value ) ) {
+						continue;
+					}
+
+					switch ( $param ) {
+						case Month::$name . '_from':
+						case Month::$name . '_to':
+							if ( $dateValue = DateTime::createFromFormat( 'Ymd', $value . '01' ) ) {
+								$dateCompare = '>=';
+								if ( substr( $param, - 3 ) === '_to' ) {
+									$dateCompare = '<=';
+									$dateValue   = DateTime::createFromFormat( 'Ymd', $dateValue->format( 'Ymt' ) );
+								}
+
+								$dateValue->setTime( 0, 0 );
+
+								if ( isset( $metaQuery[ Month::$name ] ) ) {
+									$metaQuery[ Month::$name ]['compare'] = 'BETWEEN';
+
+									if ( substr( $param, - 3 ) === '_to' ) {
+										$metaQuery[ Month::$name ]['value'] = [
+											$metaQuery[ Month::$name ]['value'],
+											$dateValue->getTimestamp()
+										];
+									} else {
+										$metaQuery[ Month::$name ]['value'] = [
+											$dateValue->getTimestamp(),
+											$metaQuery[ Month::$name ]['value']
+										];
+									}
+								} else {
+									$metaQuery[ Month::$name ] = [
+										'key'     => 'atd_cfi_sailing_date',
+										'value'   => $dateValue->getTimestamp(),
+										'type'    => 'NUMERIC',
+										'compare' => $dateCompare
+									];
+								}
+							}
+							break;
+						case DepartureType::$name:
+						case SpecialType::$name:
+						case PromoCode::$name:
+						case Destination::$name:
+						case CruiseLine::$name:
+						case Ship::$name:
+						case EmbarkPort::$name:
+						case DisembarkPort::$name:
+						case Duration::$name:
+							if ( $taxonomy === $param ) {
+								break;
+							}
+
+							$taxQuery[ $param ] = [
+								'taxonomy' => $param,
+								'terms'    => $value,
+								'field'    => 'slug',
+							];
+					}
+				}
+
+				$wpQuery = [
+					'post_type'              => Departure::$postType,
+					'posts_per_page'         => - 1,
+					'nopaging'               => true,
+					'no_found_rows'          => true,
+					'update_post_term_cache' => false,
+					'update_post_meta_cache' => false,
+					'post_status'            => 'publish',
+					'fields'                 => 'ids'
+				];
+
+				if ( ! empty( $taxQuery ) ) {
+					$wpQuery['tax_query'] = [
+						$taxQuery
+					];
+				}
+				if ( ! empty( $metaQuery ) ) {
+					$wpQuery['meta_query'] = $metaQuery;
+				}
+
+				$postQuery = new WP_Query( $wpQuery );
+
+				$termQueryParams = [
+					'taxonomy'   => $taxonomy,
+					'object_ids' => ! empty( $postQuery ) && $postQuery->post_count > 0 ? $postQuery->posts : null,
+					'childless'  => true,
+					'orderby'    => 'name',
+					'order'      => 'ASC'
+				];
+
+				if ( $taxonomy === Month::$name ) {
+					$termQueryParams = array_merge( $termQueryParams, [
+						'orderby' => 'slug'
+					] );
+				}
+
+				$termQuery          = new WP_Term_Query( $termQueryParams );
+				$terms[ $taxonomy ] = [];
+				if ( $termQuery->terms ) {
+					foreach ( $termQuery->terms as $term ) {
+						$terms[ $taxonomy ][ $term->slug ] = htmlspecialchars_decode( $term->name );
+					}
+				}
+			}
+		}
+
+		return $terms;
+	}
+
+	public function options_old( WP_REST_Request $request ): array {
+		$taxQuery                                = [];
+		$metaQuery[ Month::$name . '_baseline' ] = [
+			'key'     => 'atd_cfi_sailing_date',
+			'value'   => ( new DateTime() )->getTimestamp(),
+			'type'    => 'NUMERIC',
+			'compare' => '>='
+		];
 
 		foreach ( $request->get_query_params() as $param => $value ) {
 			if ( empty( $value ) ) {
@@ -95,12 +223,14 @@ class SearchController extends AbstractController {
 
 		if ( ! empty( $taxQuery ) || ! empty( $metaQuery ) ) {
 			$wpQuery = [
-				'post_type'      => Departure::$postType,
-				'posts_per_page' => - 1,
-				'nopaging'       => true,
-				'no_found_rows'  => true,
-				'post_status'    => 'publish',
-				'fields'         => 'ids'
+				'post_type'              => Departure::$postType,
+				'posts_per_page'         => - 1,
+				'nopaging'               => true,
+				'no_found_rows'          => true,
+				'update_post_term_cache' => false,
+				'update_post_meta_cache' => false,
+				'post_status'            => 'publish',
+				'fields'                 => 'ids'
 			];
 
 			if ( ! empty( $taxQuery ) ) {
@@ -117,9 +247,9 @@ class SearchController extends AbstractController {
 
 		$terms = [];
 		foreach ( get_object_taxonomies( Departure::$postType ) as $taxonomy ) {
-			$atd = substr( $taxonomy, 0, 3 );
+			$atd = substr( $taxonomy, 0, 11 );
 
-			if ( $atd === 'atd' ) {
+			if ( $atd === 'atd_cf_tax_' ) {
 				$termQueryParams = [
 					'taxonomy'   => $taxonomy,
 					'object_ids' => ! empty( $postQuery ) && $postQuery->post_count > 0 ? $postQuery->posts : null,
